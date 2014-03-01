1e98dc3ac983658032f4d04dcc8a7167f66a5c47
mallotore <mallotore@mallotore.(none)>
2014-03-01 14:55:24 +0100

    refactor

--- client-app/androidApp/platforms/android/assets/www/js/FAQs/faqs.js
+++ client-app/androidApp/platforms/android/assets/www/js/FAQs/faqs.js
@@ -1,33 +1,16 @@
 $(document).ready(function(){
 
-	 app.initialize();
+	app.initialize();
 
-     function FaqNavigatorInteractor(){
-
+    function FaqPanelsInteractor(){
+        var elements = ['creationPanel', 'searchPanel', 'showSearch', 'showCreate']
         var self = {};
-        var creationPanel = $(".creationPanel");
-        var searchPanel = $(".searchPanel");
-        var showSearch = $("#showSearch");
-        var showCreate = $("#showCreate");
 
-        function showWhenShouldBe(panel){
-            if(hasToBeVisible(panel)){
-                hidePanels();
-                panel.show();
-            }
-            else{
-                hidePanels();
-            }
-        }
-
-        function hasToBeVisible(element){
-            return (element.css('display') == 'none');
-        }
-
-        function hidePanels(){
-            searchPanel.hide();
-            creationPanel.hide();
-        }
+        self.init = function(){
+            initElementsByCssClass(self, elements);
+            listenOnClickEvent(self._showCreate, self._creationPanel);
+            listenOnClickEvent(self._showSearch, self._searchPanel);
+        };
 
         function listenOnClickEvent(link, panel){
             link.click(function(event){
@@ -36,55 +19,96 @@ $(document).ready(function(){
             });
         }
 
-        self.init = function(){
-            listenOnClickEvent(showCreate, creationPanel);
-            listenOnClickEvent(showSearch, searchPanel);
+        function showWhenShouldBe(panel){
+            if(hasToBeVisible(panel)){
+                hidePanels();
+                panel.show();
+                return;
+            }
+            hidePanels();
+        }
+
+        function hasToBeVisible(element){
+            return (element.css('display') == 'none');
+        }
+
+        function hidePanels(){
+            self._searchPanel.hide();
+            self._creationPanel.hide();
+        }
+
+        self.init();
+
+        return self;
+    }
+
+	function SearchEngineInteractor(){
+        var self = {};
+        var imageMapWidget = new ImageMapFilterWidget();
+        var questionsAndAnswersWidget = new QuestionsAndAnswersWidget();
+
+        imageMapWidget.init(questionsAndAnswersWidget.handler);
+
+        return self;
+	}
+
+    function QuestionsAndAnswersWidget(){
+        var elements = ['questions_answers', 'faqAnswer'];
+        var self = {};
+
+        function init(){
+            initElementsByCssClass(self, elements);
+        };
+
+        self.handler = function handler(answerText){
+            if(hasToBeVisible())
+                showAnswer(answerText);
+        };
+
+        function hasToBeVisible(){
+            return self._questions_answers.hasClass("hide");
+        }
+
+        function showAnswer(answerText){
+            self._questions_answers.removeClass("hide");
+            self._faqAnswer.text(answerText);
+        }
+
+        init();
+
+        return self;
+    }
+
+    function ImageMapFilterWidget(){
+        var elements = [ "mapzone"];
+        var self = {};
+
+        self.init = function (handler){
+            initElementsByCssClass(self, elements);
+            listenImageMapEvents(handler);
+
+            return self;
+        };
+
+        function listenImageMapEvents(handler){
+            self._mapzone.click(function(event){
+                event.preventDefault();
+                handler($(this));
+            });
         }
 
         return self;
-     }
+    }
 
-	 function HumanBodyInteractor(){
-
-		 var self = {};
-		 var mapzone = $(".mapzone");
-		 var questions_answers_container = $(".questions_answers");
-         var question = $(".respuesta");
-
-		 self._currentZone;
-
-         function refreshContentByHumanBody(){
-            questions_answers_container.removeClass("hide");
-            question.text(self._currentZone.attr("info"));
-         }
-
-         function applyVisualEffectImage(){
-           self._currentZone.maphilight({ stroke: false, fillColor: 'red', fillOpacity: 1 });
-         }
-
-		 var handler = function (){
-			 refreshContentByHumanBody();
-		     applyVisualEffectImage();
-		 };
-
-		 self.init = function (){
-			 listenImageMapEvents(handler);
-		 };
-
-		 function listenImageMapEvents(handler){
-			 mapzone.click(function(event){
-				 event.preventDefault();
-                 self._currentZone = $(this);
-				 handler();
-			 });
-		 }
-
-		 return self;
-	 }
+    function initElementsByCssClass(container, elements){
+        $.each(elements, function (index, element_id) {
+            container["_"+element_id] = $("."+element_id);
+        });
+    }
 
     function initInteractors(){
-        new HumanBodyInteractor().init();
-        new FaqNavigatorInteractor().init();
+        new SearchEngineInteractor();
+        new FaqPanelsInteractor();
     }
 
     initInteractors();
