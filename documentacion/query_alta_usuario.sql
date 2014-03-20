b57245453b1964f817af65a7ea2254f364b78be0
David Llamazares Juárez <dllamazares@atsistemas.com>
2014-03-20 20:56:20 +0100

    Renombradas unas paginas html, añadido el fichero de repartición de tareas y modificado el server.js y la exploracion fisica

--- documentacion/query_alta_usuario.sql
+++ documentacion/query_alta_usuario.sql
@@ -1,4 +1,10 @@
+# Esta crea el esquema de BBDD
+CREATE SCHEMA `db_hospital` DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci ;
+
+# Esta query te crea el usuario:d pwd:1
 insert into db_hospital.usuario (nombre, apellidos, email, usuario, pwd, rol_id) 
 values(1, 'Pepe', 'Pérez Alonso', 'dd@dd.com', 'd', 'c4ca4238a0b923820dcc509a6f75849b', 0);
 
-# Esta query te crea el usuario:d pwd:1
+
+
+
