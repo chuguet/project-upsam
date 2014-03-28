7231b3dc1740d1c5124d62401cca4914ab82325f
Carlos Huguet <huguet10@gmail.com>
2014-03-28 18:14:08 +0100

    Se reubican los metodos de ficheros en su servicio correspondiente

--- server-app/hospital.model/src/main/java/com/upsam/hospital/model/service/impl/PacienteService.java
+++ server-app/hospital.model/src/main/java/com/upsam/hospital/model/service/impl/PacienteService.java
@@ -1,27 +1,11 @@
 package com.upsam.hospital.model.service.impl;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import javax.inject.Inject;
-import javax.xml.bind.JAXBException;
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
-import com.upsam.hospital.model.beans.Exploracion3D;
-import com.upsam.hospital.model.beans.Field;
 import com.upsam.hospital.model.beans.Paciente;
-import com.upsam.hospital.model.beans.Row;
-import com.upsam.hospital.model.beans.TablaDatos;
-import com.upsam.hospital.model.beans.TipoMedida;
-import com.upsam.hospital.model.beans.UnidadMedida;
 import com.upsam.hospital.model.exceptions.DataBaseException;
-import com.upsam.hospital.model.jaxb.EmxDataFile;
-import com.upsam.hospital.model.jaxb.JaxbUtils;
 import com.upsam.hospital.model.repository.IPacienteRepository;
 import com.upsam.hospital.model.service.IPacienteService;
 
@@ -31,10 +15,6 @@ import com.upsam.hospital.model.service.IPacienteService;
  */
 @Service
 public class PacienteService implements IPacienteService {
-
-	/** The jaxb util. */
-	@Inject
-	private JaxbUtils jaxbUtil;
 
 	/** The paciente repository. */
 	@Inject
@@ -54,69 +34,6 @@ public class PacienteService implements IPacienteService {
 		catch (SQLException e1) {
 			throw new DataBaseException(e1);
 		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see
-	 * com.upsam.hospital.model.service.IPacienteService#fileReader(java.io.
-	 * File, com.upsam.hospital.model.beans.Paciente)
-	 */
-	@Override
-	public Exploracion3D fileReaderExploracion3D(File file, Paciente paciente) throws IOException {
-		List<String> lines = FileUtils.readLines(file, StandardCharsets.UTF_8);
-		Exploracion3D result = new Exploracion3D();
-		result.setPaciente(paciente);
-		Field field;
-		TablaDatos tablaDatos = new TablaDatos();
-		tablaDatos.setExploracion3D(result);
-		List<Row> rows;
-		Row row;
-		result.setFecha(new Date());
-		List<String> lineSplitted;
-		for (String line : lines) {
-			if (line.contains("Type")) {
-				lineSplitted = readLine(line);
-				result.setTipoMedida(TipoMedida.getValue(new StringBuffer(lineSplitted.get(1)).append(" ").append(lineSplitted.get(2)).toString()));
-			}
-			else if (line.contains("Measure unit")) {
-				lineSplitted = readLine(line);
-				result.setUnidadMedida(UnidadMedida.getValue(lineSplitted.get(2)));
-			}
-			else if (line.contains("Cycles")) {
-				lineSplitted = readLine(line);
-				result.setCiclos(Integer.valueOf(lineSplitted.get(1)));
-			}
-			else if (line.matches(".*\\d.*")) {
-				lineSplitted = readLine(line);
-				row = new Row();
-				rows = new ArrayList<Row>();
-				for (String split : lineSplitted) {
-					field = new Field();
-					field.setText(split);
-					field.setRow(row);
-					row.addField(field);
-					row.setTablaDatos(tablaDatos);
-					rows.add(row);
-				}
-				tablaDatos.addRow(row);
-			}
-		}
-		result.setTablaDatos(tablaDatos);
-		return result;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see
-	 * com.upsam.hospital.model.service.IPacienteService#fileReaderVideo3D(java
-	 * .io.File)
-	 */
-	@Override
-	public EmxDataFile fileReaderVideo3D(File file) throws JAXBException, IOException {
-		String fileXML = jaxbUtil.removeHeaderXmlFile(FileUtils.readFileToString(file, StandardCharsets.UTF_8));
-		EmxDataFile emxDataFile = (EmxDataFile) jaxbUtil.readXML(new ByteArrayInputStream(fileXML.getBytes(StandardCharsets.UTF_8)), EmxDataFile.class);
-		return emxDataFile;
 	}
 
 	/*
@@ -146,24 +63,6 @@ public class PacienteService implements IPacienteService {
 		catch (SQLException e1) {
 			throw new DataBaseException(e1);
 		}
-	}
-
-	/**
-	 * Read line.
-	 * 
-	 * @param line
-	 *            the line
-	 * @return the list
-	 */
-	private List<String> readLine(String line) {
-		List<String> result = new ArrayList<String>();
-		String[] stringSplitted = line.split(" ");
-		for (int i = 0; i < stringSplitted.length; i++) {
-			if (!stringSplitted[i].isEmpty()) {
-				result.add(stringSplitted[i].trim());
-			}
-		}
-		return result;
 	}
 
 	/*
