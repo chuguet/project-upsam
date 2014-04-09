a0dd799f0064f62774e1af637559727fab5e33ff
David Llamazares Juárez <dllamazares@atsistemas.com>
2014-04-09 20:06:49 +0200

    Parte cliente de recordar contraseña

---
+++ server-app/hospital.view/src/main/webapp/resources/client/js/pages/login.js
@@ -0,0 +1,31 @@
+var login = {
+				'acceso' : function(){
+					$.ajax({
+						type : 'POST',
+						url : server.URI + "j_spring_security_check",
+						data : {'j_username': $("#txtUsuario").val(), 'j_password': $("#txtPassword").val()},
+						dataType : 'json',
+						beforeSend: function (request) {
+							$.mobile.loading( 'show', {
+	  						text: 'Procesando',
+	  						textVisible: true,
+	  						theme: "b",
+	  						textonly: false
+	  						});
+			                request.setRequestHeader("X-Ajax-call", true);
+			            },
+			            complete : function(jqXHR, textStatus) {
+			            	$.mobile.loading('hide');
+	            			if (jqXHR.statusText == "No Autorizado") {
+	            				generic.alert("Error de acceso", "Usuario y/o contraseña incorrectos");
+	            			}
+	            			else if (jqXHR.statusText == "OK") {
+	            				generic.changePage('home.html');
+	            			}
+	            			else{
+	            				generic.alert("Error de servidor", "Se ha producido un error interno en el servidor");
+	            			}
+		            	}
+					});
+				}
+			}
