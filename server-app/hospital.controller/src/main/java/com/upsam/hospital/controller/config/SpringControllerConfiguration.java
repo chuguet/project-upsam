de6f1eb9de85306df2d56a3ff94ff3e5281473a3
Huguet <huguet10@gmail.com>
2014-04-22 10:44:10 +0200

    Se refactoriza la configuracion de spring mvc

--- server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/config/SpringControllerConfiguration.java
+++ server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/config/SpringControllerConfiguration.java
@@ -1,17 +1,22 @@
 package com.upsam.hospital.controller.config;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
 import com.upsam.hospital.model.config.SpringModelConfiguration;
 
 // TODO: Auto-generated Javadoc
@@ -19,6 +24,7 @@ import com.upsam.hospital.model.config.SpringModelConfiguration;
  * The Class SpringControllerConfiguration.
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan("com.upsam.hospital.controller")
 @Import(SpringModelConfiguration.class)
 public class SpringControllerConfiguration {
@@ -30,15 +36,30 @@ public class SpringControllerConfiguration {
 	 * @return the content negotiating view resolver
 	 */
 	@Bean
-	public ContentNegotiatingViewResolver contentNegotiatingViewResolver() {
-		Map<String, String> mediaTypes = new HashMap<String, String>();
-		mediaTypes.put("json", MediaType.APPLICATION_JSON_VALUE);
-		final ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
-		contentNegotiatingViewResolver.setMediaTypes(mediaTypes);
-		final ArrayList<View> defaultViews = new ArrayList<View>();
-		defaultViews.add(mappingJacksonJsonView());
+	@Inject
+	public ContentNegotiatingViewResolver contentNegotiatingViewResolver(
+			MappingJacksonJsonView mappingJacksonJsonView) {
+		ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
+		ArrayList<View> defaultViews = new ArrayList<View>();
+		defaultViews.add(mappingJacksonJsonView);
 		contentNegotiatingViewResolver.setDefaultViews(defaultViews);
 		return contentNegotiatingViewResolver;
+	}
+
+	@Bean
+	public ContentNegotiationManagerFactoryBean contentNegotiationManager() {
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager
+				.setDefaultContentType(MediaType.APPLICATION_JSON);
+		return contentNegotiationManager;
+	}
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
 	}
 
 	/**
