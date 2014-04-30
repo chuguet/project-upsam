ad527783ffa330544a250e892079a9ae80c5cebc
Huguet <huguet10@gmail.com>
2014-04-30 16:39:41 +0200

    Se añade javadoc

--- server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/dto/util/IAntecedentesPersonalesUtilDTO.java
+++ server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/dto/util/IAntecedentesPersonalesUtilDTO.java
@@ -4,12 +4,27 @@ import com.upsam.hospital.controller.dto.AntecedentesPersonalesDTO;
 import com.upsam.hospital.controller.exception.TransferObjectException;
 import com.upsam.hospital.model.beans.AntecedentesPersonales;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Interface IExploracionUtilDTO.
  */
 public interface IAntecedentesPersonalesUtilDTO {
 
+	/**
+	 * To business.
+	 *
+	 * @param antecedentesPersonalesDTO the antecedentes personales dto
+	 * @return the antecedentes personales
+	 * @throws TransferObjectException the transfer object exception
+	 */
 	AntecedentesPersonales toBusiness(AntecedentesPersonalesDTO antecedentesPersonalesDTO) throws TransferObjectException;
 
+	/**
+	 * To rest.
+	 *
+	 * @param antecedentesPersonales the antecedentes personales
+	 * @return the antecedentes personales dto
+	 * @throws TransferObjectException the transfer object exception
+	 */
 	AntecedentesPersonalesDTO toRest(AntecedentesPersonales antecedentesPersonales) throws TransferObjectException;
 }
--- server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/dto/util/IAntecedentesRelacionadosPCIUtilDTO.java
+++ server-app/hospital.controller/src/main/java/com/upsam/hospital/controller/dto/util/IAntecedentesRelacionadosPCIUtilDTO.java
@@ -4,12 +4,31 @@ import com.upsam.hospital.controller.dto.AntecedentesRelacionadosPCIDTO;
 import com.upsam.hospital.controller.exception.TransferObjectException;
 import com.upsam.hospital.model.beans.AntecedentesRelacionadosPCI;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Interface IExploracionUtilDTO.
  */
 public interface IAntecedentesRelacionadosPCIUtilDTO {
 
+	/**
+	 * To business.
+	 * 
+	 * @param antecedentesPersonalesDTO
+	 *            the antecedentes personales dto
+	 * @return the antecedentes relacionados pci
+	 * @throws TransferObjectException
+	 *             the transfer object exception
+	 */
 	AntecedentesRelacionadosPCI toBusiness(AntecedentesRelacionadosPCIDTO antecedentesPersonalesDTO) throws TransferObjectException;
 
+	/**
+	 * To rest.
+	 * 
+	 * @param antecedentesPersonales
+	 *            the antecedentes personales
+	 * @return the antecedentes relacionados pcidto
+	 * @throws TransferObjectException
+	 *             the transfer object exception
+	 */
 	AntecedentesRelacionadosPCIDTO toRest(AntecedentesRelacionadosPCI antecedentesPersonales) throws TransferObjectException;
 }
